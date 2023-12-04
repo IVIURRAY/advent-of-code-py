10ced29d0e172b40ce386882e5fdff36e48cf489
IVIURARY <haydn.murray1@yahoo.co.uk>
2023-12-04 22:03:14 +0000

    day 4 - part 2

--- day4.py
+++ day4.py
@@ -1,21 +1,43 @@
 if __name__ == '__main__':
     result = 0
+    copies = []
+    cards = {}
+
     with open('day4.txt') as f:
-        for line in f:
+        for i, line in enumerate(f, start=1):
             game_id, game = line.split(':')
             winning, numbers = game.split("|")
             winning = winning.strip().split()
             numbers = numbers.strip().split()
 
-            total = 0
+            count = 0
+            result += 1
             for number in numbers:
-                if number in winning:
-                    if total == 0:
-                        total = 1
-                    else:
-                        total *= 2
 
-            result += total
-            print(game_id, total)
+                if number in winning:
+                    count += 1
+
+            print(game_id, count)
+            cards[i] = {'id': i, 'winning': winning, 'numbers': numbers, 'won': count}
+
+        for game_id, game in cards.items():
+            count = game['won']
+            while count > 0:
+                result += 1
+                print(f'{game_id}: Adding {game_id + count} to copies')
+                copies.append(cards[game_id + count])
+                count -= 1
+
+        while copies:
+            game = copies.pop(0)
+            game_id = game['id']
+            count = game['won']
+
+            while count > 0:
+                result += 1
+                print(f'{game_id}: Adding {game_id + count} to copies')
+                copies.append(cards[game_id + count])
+                count -= 1
+
 
         print(result)
